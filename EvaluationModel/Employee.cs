40622416f681ffa2f8a791773b48ff619eb502eb
Kenji Okamoto <kenjio@users.noreply.github.com>
2016-03-09 13:29:07 -0800

    Add attributes to the Employee model

--- EvaluationModel/Employee.cs
+++ EvaluationModel/Employee.cs
@@ -8,5 +8,27 @@ namespace Evaluation.Model
 {
     public class Employee
     {
+        public int employeeId { get { return _id; } }
+        public string firstName { get { return _firstName; } }
+        public string lastName { get { return _lastName; } }
+        public string email { get { return _email; } }
+        public Boolean isAdmin { get { return _isAdmin; } }
+        public int? cohortId { get; set; }
+        public int? supervisorId { get; set; }
+
+        public Employee(int id, string firstName, string lastName, string email, Boolean isAdmin)
+        {
+            _id = id;
+            _firstName = firstName;
+            _lastName = lastName;
+            _email = email;
+            _isAdmin = isAdmin;
+        }
+
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private Boolean _isAdmin;
     }
 }
