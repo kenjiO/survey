3e9fbb17b760acf413cf8dfbdf12029a2fe937dc
Ron Struempf <rstruem1@my.westga.edu>
2016-03-09 22:04:40 -0500

    Had to manually add my unit test file

---
+++ UnitTests/SampleUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Evaluation.Model;
+using Test;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// Testing TestController to get unit tests started
+    /// </summary>
+    [TestClass]
+    public class TestControllerTest
+    {
+        TestController controller = new TestController();
+
+        [TestMethod]
+        public void whenGetStageListShouldHave5Stages()
+        {
+            List<Stage> list = controller.getStageList();
+            Assert.AreEqual(5, list.Count);
+        }
+    }
+}
