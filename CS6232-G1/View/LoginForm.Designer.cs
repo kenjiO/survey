23f57ac252780f7aa123c0411e9fd47edb59fe6a
Kenji Okamoto <kenjio@users.noreply.github.com>
2016-03-11 10:08:40 -0800

    Create Form for login

---
+++ CS6232-G1/View/LoginForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace CS6232_G1.View
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ErrorMsgLabel = new System.Windows.Forms.Label();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // ErrorMsgLabel
+            // 
+            this.ErrorMsgLabel.ForeColor = System.Drawing.Color.Red;
+            this.ErrorMsgLabel.Location = new System.Drawing.Point(10, 18);
+            this.ErrorMsgLabel.Name = "ErrorMsgLabel";
+            this.ErrorMsgLabel.Size = new System.Drawing.Size(305, 13);
+            this.ErrorMsgLabel.TabIndex = 2;
+            this.ErrorMsgLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Location = new System.Drawing.Point(111, 57);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(155, 20);
+            this.UsernameTextBox.TabIndex = 3;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(111, 90);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '*';
+            this.PasswordTextBox.Size = new System.Drawing.Size(155, 20);
+            this.PasswordTextBox.TabIndex = 4;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(147, 128);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.TabIndex = 5;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 191);
+            this.ControlBox = false;
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UsernameTextBox);
+            this.Controls.Add(this.ErrorMsgLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Login";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ErrorMsgLabel;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Button LoginButton;
+    }
+}
---
+++ CS6232-G1/View/LoginForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Evaluation.Controller;
+using Evaluation.Model;
+
+namespace CS6232_G1.View
+{
+    public partial class LoginForm : Form
+    {
+        IEvaluationController _controller;
+
+        public LoginForm(IEvaluationController controller)
+        {
+            _controller = controller;
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            String username = UsernameTextBox.Text;
+            String password = PasswordTextBox.Text;
+            if (!Validate(username, password))
+                return;
+
+            if (_controller.login(username, password) == null)
+            {
+                ErrorMsgLabel.Text = "Invalid username/password";
+                return;
+            }
+
+            if (_controller.idAdminSession)
+            {
+                ErrorMsgLabel.Text = "TODO: Start admin session";
+            }
+            else
+            {
+                ErrorMsgLabel.Text = "TODO: Start user session";
+            }
+        }
+
+        private Boolean Validate(String username, String password)
+        {
+            if (username != null && username != "" && password != null && password != "")
+            {
+                return true;
+            }
+            else
+            {
+                ErrorMsgLabel.Text = "Username and Password required";
+                return false;
+            }
+        }
+    }
+}
