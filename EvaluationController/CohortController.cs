93fc3ba821ae8496c69412763839e6bf010ff3c8
Betsy Louis <blouis1@my.westga.edu>
2016-03-15 15:12:26 -0400

    Removed class EvaluationScheduleDAL. Moved method to CohortDAL. Renamed EvaluationScheduleController to CohortController.

---
+++ EvaluationController/CohortController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using EvaluationModel;
+
+namespace Evaluation.Controller
+{
+    /// <summary>
+    /// Evaluation Schedule View-Controller
+    /// </summary>
+    public partial class EvaluationController : IEvaluationController
+    {
+        public List<EvaluationSchedule> getEvaluationScheduleList(int cohortId)
+        {
+            return _dal.getEvaluationScheduleList(cohortId);
+        }
+    }
+}
