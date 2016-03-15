4c76ab14ee1b7c30a6a53604eb08f77a92d73e75
Betsy Louis <blouis1@my.westga.edu>
2016-03-15 18:12:56 -0400

    Added test controller methods and unit tests.

--- Test/TestController.cs
+++ Test/TestController.cs
@@ -97,9 +97,18 @@ namespace Test
         public List<EvaluationSchedule> getEvaluationScheduleList(int cohortId)
         {
             List<EvaluationSchedule> results = new List<EvaluationSchedule>();
-
-            results.Add(new EvaluationSchedule(1, 1, 1, DateTime.Now, Convert.ToDateTime("31/10/2016")));
-            results.Add(new EvaluationSchedule(1, 2, 1, DateTime.Now, Convert.ToDateTime("31/10/2016")));
+            switch (cohortId)
+            {
+                case 1:
+                    results.Add(new EvaluationSchedule(1, 1, 1, DateTime.Now, DateTime.Parse("2016-06-10")));
+                    results.Add(new EvaluationSchedule(1, 2, 1, DateTime.Now, DateTime.Parse("2016-06-10")));
+                    break;
+                case 2:
+                    results.Add(new EvaluationSchedule(2, 1, 1, DateTime.Now, DateTime.Parse("2016-06-10")));
+                    break;
+                default:
+                    throw new KeyNotFoundException("Invalid cohort id, " + cohortId);
+            }
             return results;
         }
         #endregion
--- UnitTests/SampleUnitTest.cs
+++ UnitTests/SampleUnitTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using Evaluation.Model;
 using Test;
+using EvaluationModel;
 
 namespace UnitTests
 {
@@ -19,6 +20,32 @@ namespace UnitTests
         {
             List<Stage> list = controller.getStageList();
             Assert.AreEqual(5, list.Count);
+        }
+
+        [TestMethod]
+        public void whenGetTypeNameShouldGiveType1()
+        {
+            Assert.AreEqual("Type 1", controller.getTypeName(1));
+        }
+
+        [TestMethod]
+        public void whenGetTypeNameShouldGiveType2()
+        {
+            Assert.AreEqual("Type 2", controller.getTypeName(2));
+        }
+
+        [TestMethod]
+        public void whenGetEvaluationScheduleListShouldHave2EvaluationsForCohort1()
+        {
+            List<EvaluationSchedule> list = controller.getEvaluationScheduleList(1);
+            Assert.AreEqual(2, list.Count);
+        }
+
+        [TestMethod]
+        public void whenGetEvaluationScheduleListShouldHave2EvaluationsForCohort2()
+        {
+            List<EvaluationSchedule> list = controller.getEvaluationScheduleList(2);
+            Assert.AreEqual(1, list.Count);
         }
     }
 }
