ecdf2fad2da97a3f9c265b1dd44441e44b10cf60
Betsy Louis <blouis1@my.westga.edu>
2016-03-15 12:37:48 -0400

    Added model class EvaluationSchedule.

---
+++ EvaluationModel/EvaluationSchedule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluationModel
+{
+    public class EvaluationSchedule
+    {
+        public int cohortId { get; set; }
+        public string cohortName { get; set; }
+        public int typeId { get; set; }
+        public string typeName { get; set; }
+        public int stageId { get; set; }
+        public string stageName { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+    }
+}
