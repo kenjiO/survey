2a3898df39dad47af7a2d421bf62effda1db4fd7
Ron Struempf <rstruem1@my.westga.edu>
2016-03-19 11:00:58 -0400

    Added unit tests for StageController (stage portion of controller)

---
+++ UnitTests/StageControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Evaluation.Controller;
+using Test;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class StageControllerTest
+    {
+        IEvaluationController controller = new EvaluationController(new TestDAL());
+
+        [TestMethod]
+        public void whenStageExistsStage1ShouldReturnTrue()
+        {
+            Assert.IsTrue(controller.stageExists("Stage 1"));
+        }
+
+        [TestMethod]
+        public void whenStageExistsStage100ShouldReturnFalse()
+        {
+            Assert.IsFalse(controller.stageExists("Stage 100"));
+        }
+
+        [TestMethod]
+        public void whenGetStageNameForId1ShouldGetStage1()
+        {
+            Assert.AreEqual("Stage 1", controller.getStageName(1));
+        }
+
+        [TestMethod]
+        public void whenGetStageNameForId100ShouldGetEmptyString()
+        {
+            Assert.AreEqual("", controller.getStageName(100));
+        }
+
+    }
+}
