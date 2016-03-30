61562791edb716f5c9f64fd9c087c8912b8f5fbe
Ron Struempf <rstruem1@my.westga.edu>
2016-03-29 20:49:28 -0400

    Didn't get new views file pulled into git

---
+++ database/evaluations_view.sql
@@ -0,0 +1,17 @@
+GO
+DROP VIEW dbo.EvaluationScheduleView
+GO
+CREATE VIEW dbo.EvaluationScheduleView
+AS
+SELECT  es.scheduleId,
+        es.cohortId,
+        es.typeId,
+        ty.typeName,
+        ty.answerRange,
+        es.stageId,
+        st.stageName,
+        startDate,
+        endDate
+  FROM dbo.evaluation_schedule es
+  LEFT JOIN dbo.type ty ON (ty.typeId = es.typeId)
+  LEFT JOIN dbo.stage st ON (st.stageId = es.stageId)
