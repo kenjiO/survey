e07fdbe1a3acb7920eb2be1601cbe6db10b7e8af
Ron Struempf <rstruem1@my.westga.edu>
2016-03-31 18:20:42 -0400

    Added DateRange object

---
+++ EvaluationModel/DateRange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation.Model
+{
+    public class DateRange
+    {
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+
+        public DateRange(DateTime start, DateTime end)
+        {
+            startDate = start;
+            endDate = end;
+        }
+    }
+}
