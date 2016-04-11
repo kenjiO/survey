8412de72296087c08339bea450dc65b8eef294db
Ron Struempf <rstruem1@my.westga.edu>
2016-04-10 20:43:10 -0400

    Committing before trying to update report viewer

---
+++ EvaluationModel/UserReportTitleData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation.Model
+{
+    public class UserReportTitleData
+    {
+        public string Name { get; private set; }
+        public string TypeName { get; private set; }
+        public string StageName { get; private set; }
+
+        public UserReportTitleData(string _name, string _type, string _stage)
+        {
+            Name = _name;
+            TypeName = _type;
+            StageName = _stage;
+        }
+    }
+}
