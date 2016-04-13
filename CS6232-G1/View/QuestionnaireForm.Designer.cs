ee838c34cf5b3f710f35634057417b0476ddcc59
Kenji Okamoto <kenjio@users.noreply.github.com>
2016-04-12 22:02:19 -0700

    Increase height on questionnaire form to make it more user friendly and still fit on a 1280x720 screen

--- CS6232-G1/View/QuestionnaireForm.Designer.cs
+++ CS6232-G1/View/QuestionnaireForm.Designer.cs
@@ -46,9 +46,10 @@
             this.lblGeneral.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.lblGeneral.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lblGeneral.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblGeneral.Location = new System.Drawing.Point(32, 96);
+            this.lblGeneral.Location = new System.Drawing.Point(21, 62);
+            this.lblGeneral.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblGeneral.Name = "lblGeneral";
-            this.lblGeneral.Size = new System.Drawing.Size(851, 70);
+            this.lblGeneral.Size = new System.Drawing.Size(568, 46);
             this.lblGeneral.TabIndex = 3;
             this.lblGeneral.Text = "Text";
             this.lblGeneral.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -56,18 +57,20 @@
             // lblInstructions
             // 
             this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblInstructions.Location = new System.Drawing.Point(381, 189);
+            this.lblInstructions.Location = new System.Drawing.Point(254, 123);
+            this.lblInstructions.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblInstructions.Name = "lblInstructions";
-            this.lblInstructions.Size = new System.Drawing.Size(100, 110);
+            this.lblInstructions.Size = new System.Drawing.Size(67, 71);
             this.lblInstructions.TabIndex = 4;
             this.lblInstructions.Text = "Instructions";
             // 
             // lblTitle
             // 
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(243, 32);
+            this.lblTitle.Location = new System.Drawing.Point(162, 21);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(566, 41);
+            this.lblTitle.Size = new System.Drawing.Size(377, 27);
             this.lblTitle.TabIndex = 5;
             this.lblTitle.Text = "Performance Evaluation Questionnaire";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -76,10 +79,10 @@
             // 
             this.lblEmployeeName.AutoSize = true;
             this.lblEmployeeName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEmployeeName.Location = new System.Drawing.Point(39, 303);
-            this.lblEmployeeName.Margin = new System.Windows.Forms.Padding(30, 0, 3, 0);
+            this.lblEmployeeName.Location = new System.Drawing.Point(26, 197);
+            this.lblEmployeeName.Margin = new System.Windows.Forms.Padding(20, 0, 2, 0);
             this.lblEmployeeName.Name = "lblEmployeeName";
-            this.lblEmployeeName.Size = new System.Drawing.Size(223, 29);
+            this.lblEmployeeName.Size = new System.Drawing.Size(150, 20);
             this.lblEmployeeName.TabIndex = 6;
             this.lblEmployeeName.Text = "Peer Review For: ";
             this.lblEmployeeName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -88,10 +91,10 @@
             // 
             this.lblDate.AutoSize = true;
             this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDate.Location = new System.Drawing.Point(809, 303);
-            this.lblDate.Margin = new System.Windows.Forms.Padding(30, 0, 3, 0);
+            this.lblDate.Location = new System.Drawing.Point(539, 197);
+            this.lblDate.Margin = new System.Windows.Forms.Padding(20, 0, 2, 0);
             this.lblDate.Name = "lblDate";
-            this.lblDate.Size = new System.Drawing.Size(81, 29);
+            this.lblDate.Size = new System.Drawing.Size(58, 20);
             this.lblDate.TabIndex = 8;
             this.lblDate.Text = "Date: ";
             this.lblDate.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -100,10 +103,10 @@
             // 
             this.lblEvaluator.AutoSize = true;
             this.lblEvaluator.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEvaluator.Location = new System.Drawing.Point(39, 353);
-            this.lblEvaluator.Margin = new System.Windows.Forms.Padding(30, 0, 3, 0);
+            this.lblEvaluator.Location = new System.Drawing.Point(26, 229);
+            this.lblEvaluator.Margin = new System.Windows.Forms.Padding(20, 0, 2, 0);
             this.lblEvaluator.Name = "lblEvaluator";
-            this.lblEvaluator.Size = new System.Drawing.Size(136, 29);
+            this.lblEvaluator.Size = new System.Drawing.Size(95, 20);
             this.lblEvaluator.TabIndex = 9;
             this.lblEvaluator.Text = "Evaluator: ";
             this.lblEvaluator.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -112,10 +115,10 @@
             // 
             this.lblRole.AutoSize = true;
             this.lblRole.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRole.Location = new System.Drawing.Point(692, 353);
-            this.lblRole.Margin = new System.Windows.Forms.Padding(30, 0, 3, 0);
+            this.lblRole.Location = new System.Drawing.Point(461, 229);
+            this.lblRole.Margin = new System.Windows.Forms.Padding(20, 0, 2, 0);
             this.lblRole.Name = "lblRole";
-            this.lblRole.Size = new System.Drawing.Size(198, 29);
+            this.lblRole.Size = new System.Drawing.Size(137, 20);
             this.lblRole.TabIndex = 10;
             this.lblRole.Text = "Evaluator Role: ";
             this.lblRole.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -125,10 +128,10 @@
             this.submitPanel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.submitPanel.Controls.Add(this.lblSubmitNotice);
             this.submitPanel.Controls.Add(this.btnSubmit);
-            this.submitPanel.Location = new System.Drawing.Point(57, 443);
-            this.submitPanel.Margin = new System.Windows.Forms.Padding(3, 3, 3, 30);
+            this.submitPanel.Location = new System.Drawing.Point(38, 288);
+            this.submitPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 19);
             this.submitPanel.Name = "submitPanel";
-            this.submitPanel.Size = new System.Drawing.Size(839, 135);
+            this.submitPanel.Size = new System.Drawing.Size(559, 88);
             this.submitPanel.TabIndex = 7;
             // 
             // lblSubmitNotice
@@ -136,9 +139,9 @@
             this.lblSubmitNotice.Dock = System.Windows.Forms.DockStyle.Top;
             this.lblSubmitNotice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblSubmitNotice.Location = new System.Drawing.Point(0, 0);
-            this.lblSubmitNotice.Margin = new System.Windows.Forms.Padding(3);
+            this.lblSubmitNotice.Margin = new System.Windows.Forms.Padding(2);
             this.lblSubmitNotice.Name = "lblSubmitNotice";
-            this.lblSubmitNotice.Size = new System.Drawing.Size(839, 79);
+            this.lblSubmitNotice.Size = new System.Drawing.Size(559, 51);
             this.lblSubmitNotice.TabIndex = 1;
             this.lblSubmitNotice.Text = "Your answers are automatically saved. You may close your form and come back to it" +
     " as many times as you wish. However, once you submit, this evaluation will be cl" +
@@ -149,9 +152,10 @@
             // 
             this.btnSubmit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSubmit.Location = new System.Drawing.Point(695, 85);
+            this.btnSubmit.Location = new System.Drawing.Point(463, 55);
+            this.btnSubmit.Margin = new System.Windows.Forms.Padding(2);
             this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(131, 47);
+            this.btnSubmit.Size = new System.Drawing.Size(87, 31);
             this.btnSubmit.TabIndex = 0;
             this.btnSubmit.Text = "Submit";
             this.btnSubmit.UseVisualStyleBackColor = true;
@@ -159,11 +163,11 @@
             // 
             // QuestionnaireForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.AutoScrollMargin = new System.Drawing.Size(0, 30);
-            this.ClientSize = new System.Drawing.Size(914, 644);
+            this.ClientSize = new System.Drawing.Size(609, 561);
             this.Controls.Add(this.lblRole);
             this.Controls.Add(this.lblEvaluator);
             this.Controls.Add(this.lblDate);
@@ -172,6 +176,7 @@
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.lblInstructions);
             this.Controls.Add(this.lblGeneral);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "QuestionnaireForm";
             this.Text = "Evaluation Questionnaire";
             this.Load += new System.EventHandler(this.QuestionnaireForm_Load);
